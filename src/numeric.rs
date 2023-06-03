1b2fed743743ee44866a68f58445e0a6c3d289d1
DaveatCor <condaveat@gmail.com>
2023-06-03 11:17:19 +0700

    init project

--- src/numeric.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused)]
-
-fn numeric(){
-
-    let a = 150u32;
-    let b: i8 = 10i8;
-
-    println!("{}", a + 8);
-}
--- src/vec.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) fn vec_function() -> Vec<String>{
-
-  let vc = vec!["a".to_string(), "b".to_string(), "c".to_string()];
-  return vc;
-}
